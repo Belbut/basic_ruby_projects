7d82665d91f7fa796377d46f6587b12054cd5ec5
Belbut <80793911+Belbut@users.noreply.github.com>
2024-03-20 21:48:20 +0000

    Complete caesar cipher prototype

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,9 +1,29 @@
-def caesar_cipher(string, shift_factor=0)
+def caesar_cipher(message, shift_factor = 0)
+  message_splited_code = message.codepoints
+  cipher_splited_code = message_splited_code.map do |code|
+    changed_code = code + shift_factor
+    case code
+    when (65..90)  # a->z
+      alphabetic_rebase(changed_code, 65, 90)
+    when (97..122) # A->Z
+      alphabetic_rebase(changed_code, 97, 122)
+    else
+      code
+    end
+  end
 
-    cipher = string
-    puts cipher
+  puts cipher_splited_code.map(&:chr).join
 end
 
-puts "Should be equal:"
-puts "Bmfy f xywnsl!"
-caesar_cipher("What a string!", 5)
+NUMBER_OF_LETTERS_IN_ALPHABET = 26
+def alphabetic_rebase(changed_code, lowest_code, _highest_code)
+  changed_code -= lowest_code
+  letter_of_alph = changed_code % NUMBER_OF_LETTERS_IN_ALPHABET
+  letter_of_alph + lowest_code
+end
+
+puts 'Bmfy f xywnsl!'
+caesar_cipher('What a string!', 5)
+a = 'a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z'
+puts a
+caesar_cipher(a, 1)
