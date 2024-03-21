443eddd68cc834608ec146e1bbca9577e535e0c0
Belbut <80793911+Belbut@users.noreply.github.com>
2024-03-21 17:46:09 +0000

    Create tests for stock_picker project

---
+++ stock_picker.rb
@@ -0,0 +1,13 @@
+def stock_picker(array)end
+test1 = [1, 4]
+test2 = [1, 6]
+test3 = [0, 1]
+puts '-------------test1--------------'
+p test1
+p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
+puts '-------------test2--------------'
+p test2
+p stock_picker([17, 3, 6, 9, 15, 8, 60, 1])
+puts '-------------test3--------------'
+p test3
+p stock_picker([1, 2, 1, 2, 1])
