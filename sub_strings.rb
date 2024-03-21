4a42fd23e18fc9adeee29036c1b49634731a28ef
Belbut <80793911+Belbut@users.noreply.github.com>
2024-03-21 16:26:52 +0000

    Complete sub_string solution

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,10 +1,14 @@
-def substrings(str, dictionary); end
+def substrings(str, dictionary)
+  dictionary.each_with_object(Hash.new(0)) do |word, acumulator|
+    regex = /#{Regexp.escape(word)}/i
+    acumulator[word] = str.scan(regex).size if str.scan(regex).any?
+  end
+end
 
 test1 = { 'below' => 1, 'low' => 1 }
 test2 = { 'down' => 1, 'go' => 1, 'going' => 1, 'how' => 2, 'howdy' => 1, 'it' => 2, 'i' => 3, 'own' => 1, 'part' => 1,
           'partner' => 1, 'sit' => 1 }
 dictionary = %w[below down go going horn how howdy it i low own part partner sit]
-
 puts '-------------test2--------------'
 puts test1
 puts substrings('below', dictionary)
