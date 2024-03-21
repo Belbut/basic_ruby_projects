d430f515f278b182c61e3ea90bcb26c71471528d
Belbut <80793911+Belbut@users.noreply.github.com>
2024-03-21 12:21:43 +0000

    Refactor code

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,25 +1,45 @@
-def caesar_cipher(message, shift_factor = 0)
-  message_splited_code = message.codepoints
-  cipher_splited_code = message_splited_code.map do |code|
-    changed_code = code + shift_factor
-    case code
-    when (65..90)  # a->z
-      alphabetic_rebase(changed_code, 65, 90)
-    when (97..122) # A->Z
-      alphabetic_rebase(changed_code, 97, 122)
-    else
-      code
-    end
-  end
+NUMBER_OF_LETTERS_IN_ALPHABET = 26
+RANGE_UPPERCASE_LETTERS = (97..122).freeze
+RANGE_LOWERCASE_LETTERS = (65..90).freeze
+UPPERCASE_FIRST_LETTER = 97
+LOWERCASE_FIRST_LETTER = 65
 
-  puts cipher_splited_code.map(&:chr).join
+def alphabetic_wrap(char_decimal_shifted, case_constant)
+  # rebases upcase and lowcase to the same axis
+  char_decimal_shifted -= case_constant
+  # removes the number of wraps because of the shift
+  cipher_char = char_decimal_shifted % NUMBER_OF_LETTERS_IN_ALPHABET
+  # returns the chars to the same up/low case axis
+  cipher_char + case_constant
 end
 
-NUMBER_OF_LETTERS_IN_ALPHABET = 26
-def alphabetic_rebase(changed_code, lowest_code, _highest_code)
-  changed_code -= lowest_code
-  letter_of_alph = changed_code % NUMBER_OF_LETTERS_IN_ALPHABET
-  letter_of_alph + lowest_code
+def dec_mess_to_cipher(chr_code, shift_factor)
+  cript_chr_code = chr_code + shift_factor
+
+  case chr_code
+  when RANGE_UPPERCASE_LETTERS
+    upcase_rebase(cript_chr_code)
+  when RANGE_LOWERCASE_LETTERS
+    lowcase_rebase(cript_chr_code)
+  else
+    chr_code
+  end
+end
+
+def upcase_rebase(cript_chr_code)
+  alphabetic_wrap(cript_chr_code, UPPERCASE_FIRST_LETTER)
+end
+
+def lowcase_rebase(cript_chr_code)
+  alphabetic_wrap(cript_chr_code, LOWERCASE_FIRST_LETTER)
+end
+
+def caesar_cipher(message, shift_factor = 0)
+  splited_dec_message = message.codepoints
+
+  splited_dec_cipher = splited_dec_message.map { |code| dec_mess_to_cipher(code, shift_factor) }
+
+  puts splited_dec_cipher.map(&:chr).join
 end
 
 puts 'Bmfy f xywnsl!'
