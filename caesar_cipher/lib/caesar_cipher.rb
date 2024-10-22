c24efe513569de9c8d2498613644907ea0bc4489
Belbut <80793911+Belbut@users.noreply.github.com>
2024-10-22 18:04:29 +0100

    Refactor and rename existing logic

--- caesar_cipher/lib/caesar_cipher.rb
+++ caesar_cipher/lib/caesar_cipher.rb
@@ -1,20 +1,34 @@
 # frozen_string_literal: true
 
 class CaesarCipher
-  def initialize(message, shift_factor = 0)
+  NUMBER_OF_LETTERS_IN_ALPHABET = 26
+  RANGE_UPPERCASE_LETTERS = (65..90).freeze
+  RANGE_LOWERCASE_LETTERS = (97..122).freeze
+  UPPERCASE_FIRST_LETTER = 65
+  LOWERCASE_FIRST_LETTER = 97
+
+  def initialize(message, shift_factor = 1)
     @message = message
     @shift_factor = shift_factor
   end
 
-  def upcase_rebase(cipher_chr_code)
-    alphabetic_wrap(cipher_chr_code, UPPERCASE_FIRST_LETTER)
+  def caesar_cipher
+    splitted_dec_message = @message.codepoints
+
+    splinted_dec_cipher = splitted_dec_message.map { |code| CaesarCipher.dec_msg_to_cipher(code, @shift_factor) }
+
+    splinted_dec_cipher.map(&:chr).join
   end
 
-  def lowcase_rebase(cipher_chr_code)
-    alphabetic_wrap(cipher_chr_code, LOWERCASE_FIRST_LETTER)
+  def self.upcase_rebase(number_to_wrap)
+    alphabetic_wrap(number_to_wrap, UPPERCASE_FIRST_LETTER)
   end
 
-  def dec_msg_to_cipher(chr_code, shift_factor)
+  def self.lowcase_rebase(number_to_wrap)
+    alphabetic_wrap(number_to_wrap, LOWERCASE_FIRST_LETTER)
+  end
+
+  def self.dec_msg_to_cipher(chr_code, shift_factor)
     cipher_chr_code = chr_code + shift_factor
 
     case chr_code
@@ -27,19 +41,7 @@ class CaesarCipher
     end
   end
 
-  def caesar_cipher
-    splitted_dec_message = @message.codepoints
-
-    splinted_dec_cipher = splitted_dec_message.map { |code| dec_msg_to_cipher(code, @shift_factor) }
-
-    cipher = splinted_dec_cipher.map(&:chr).join
-
-    puts cipher
-  end
-
-  private
-
-  def alphabetic_wrap(char_dec_shifted, first_letter_up_low_case_code)
+  def self.alphabetic_wrap(char_dec_shifted, first_letter_up_low_case_code)
     # rebases upcase and lowcase to the same axis of alphabet eg a/A = 0 b/B=1
     char_dec_shifted -= first_letter_up_low_case_code
     # removes the number of wraps because of the shift
@@ -47,12 +49,6 @@ class CaesarCipher
     # returns the chars to the same up/low case axis
     alphabet_letter_dec_code + first_letter_up_low_case_code
   end
-
-  NUMBER_OF_LETTERS_IN_ALPHABET = 26
-  RANGE_UPPERCASE_LETTERS = (97..122).freeze
-  RANGE_LOWERCASE_LETTERS = (65..90).freeze
-  UPPERCASE_FIRST_LETTER = 97
-  LOWERCASE_FIRST_LETTER = 65
 end
 # puts 'Bmfy f xywnsl!'
 # caesar_cipher('What a string!', 5)
